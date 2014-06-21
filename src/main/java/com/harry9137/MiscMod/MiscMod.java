e7e389a49983cb306628c20beecc549b5ff0f006
Harry9137 <6billionthusernametry@gmail.com>
2014-06-21 09:07:32 +0200

    Minecraft Forge Installation along with a main mod class

---
+++ src/main/java/com/harry9137/MiscMod/MiscMod.java
@@ -0,0 +1,31 @@
+package com.harry9137.MiscMod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.*;
+
+@Mod(
+        modid="MiscMod",
+        name="Misc Mod",
+        version="1.7.2-1.0"
+)
+public class MiscMod
+{
+    @Mod.Instance("MiscMod")
+    public static MiscMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
